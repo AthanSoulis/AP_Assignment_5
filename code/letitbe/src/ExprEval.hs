9a50e516a3dd78cd92a5ce80e4ae2150ce9e8abe
Yannick-Neubert <yannick.neubert@gmx.de>
2022-10-11 22:12:08 +0200

    wip going to bed

--- code/letitbe/src/ExprEval.hs
+++ code/letitbe/src/ExprEval.hs
@@ -12,7 +12,7 @@ oper Minus = (-)
 oper Times = (*)
 
 eval :: Expr -> Env -> Either String Int
-eval (Const n) env = return n
+eval (Const n) _ = return n
 eval (Oper op x y) env = (oper op) <$> eval x env <*> eval y env
 eval (Var v) env = case M.lookup v env of
                      Nothing -> Left ("Unknown identifier: "++v)
@@ -21,14 +21,24 @@ eval (Let v e body) env = do
   val <- eval e env
   eval body $ M.insert v val env
 
+evalTop :: Expr -> Either String Int
 evalTop e = eval e M.empty
 
+simplify :: Expr -> Expr
 simplify e =
   case e of
+    Oper Plus (Const 0) x -> x
+    Oper Times (Const 0) _ -> (Const 0)
+    Oper Times (Const 1) x -> x
     Oper Plus (Const c1) (Const c2) -> Const(c1+c2)
     Oper Minus (Const c1) (Const c2) -> Const(c1-c2) -- bug was here
     Oper Times (Const c1) (Const c2) -> Const(c1*c2)
     Oper op e1 e2 -> Oper op (simplify e1) (simplify e2)
     Let v e body ->
-      Let v (simplify e) (simplify body)
+      if usesVar v body then Let v (simplify e) (simplify body) else simplify body where
+          usesVar v body = case body of 
+            Var z -> v == z
+            Oper _ e1 e2 -> usesVar v e1 || usesVar v e2
+            Let _ e1 e2 -> usesVar v e1 || usesVar v e2
+            _ -> False
     _ -> e
--- code/letitbe/tests/ExprProperties.hs
+++ code/letitbe/tests/ExprProperties.hs
@@ -5,14 +5,16 @@ import Test.QuickCheck
 import ExprAst
 import qualified ExprEval as E
 
+import Data.Either
+
 exprN :: Int -> Gen Expr
-exprN 0 = oneof [fmap Const arbitrary, fmap Var arbitrary]
+exprN 0 = oneof [fmap Const arbitrary, fmap Var $ vectorOf 5 $ elements ['A'..'Z']]
 exprN n = oneof 
    [fmap Const arbitrary
-   , fmap Var arbitrary
+   , fmap Var $ vectorOf 5 $ elements ['A'..'Z']
    , do x <- exprN (n `div` 2)
         y <- exprN (n `div` 2)
-        a <- arbitrary
+        a <- vectorOf 5 $ elements ['A'..'Z']
         z <- elements [
          Oper Plus x y
          , Oper Minus x y
@@ -21,8 +23,21 @@ exprN n = oneof
         return $ z
    ]
 
+data UpperCaseString = UpperCaseString String
+
+instance Arbitrary UpperCaseString where
+   arbitrary = do
+      n <- chooseInt (0, 5)
+      str <- vectorOf n (elements ['A'..'Z'])
+      return $ UpperCaseString str
+
 instance Arbitrary Expr where
    arbitrary = sized exprN
 
 prop_eval_simplify :: Expr -> Property
-prop_eval_simplify x = (E.evalTop $ E.simplify x) === (E.evalTop x)
+prop_eval_simplify x = --E.evalTop x === (E.evalTop $ E.simplify x)
+   let simp = (E.simplify x) in let res = (E.evalTop x) in case simp of
+   (Const 0) -> True === True                   -- evalTop can be whatever, including Left _
+   _         -> case res of
+                  Left _ ->  (isLeft $ E.evalTop simp) === True
+                  Right _ -> E.evalTop x === (E.evalTop simp)
