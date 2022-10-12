0551c60824c83bb1dc47ecdf9769b8f2bb639797
Yannick-Neubert <yannick.neubert@gmx.de>
2022-10-12 14:05:14 +0200

    add comments

--- code/letitbe/tests/ExprProperties.hs
+++ code/letitbe/tests/ExprProperties.hs
@@ -5,8 +5,6 @@ import Test.QuickCheck
 import ExprAst
 import qualified ExprEval as E
 
-import Data.Either
-
 exprN :: Int -> Gen Expr
 exprN 0 = oneof [fmap Const arbitrary, fmap Var arbitrary]
 exprN n = oneof 
@@ -16,10 +14,10 @@ exprN n = oneof
         y <- exprN (n `div` 2)
         a <- arbitrary
         z <- elements [
-         Oper Plus x y
-         , Oper Minus x y
-         , Oper Times x y
-         , Let a x y] --chances of actually using a inside y are astronomically low
+            Oper Plus x y
+            , Oper Minus x y
+            , Oper Times x y
+            , Let a x y] --chances of actually using a inside y are astronomically low
         return $ z
    ]
 
@@ -29,12 +27,7 @@ instance Arbitrary Expr where
 prop_eval_simplify :: Expr -> Property
 prop_eval_simplify x = --E.evalTop x === (E.evalTop $ E.simplify x) but:
    --simplify can eval expr that evalTop cant
+   --specifically multiplication with 0 and unused let binding
    let simp = (E.simplify x) in let res = (E.evalTop x) in case res of
          Left _ -> True === True --no way to check this
          Right _ -> E.evalTop simp === res
-
-
-   -- (Const 0) -> True === True                   -- evalTop can be whatever, including Left _
-   -- _         -> case res of
-   --                Left _ ->  (isLeft $ E.evalTop simp) === True
-   --                Right _ -> E.evalTop x === (E.evalTop simp)
