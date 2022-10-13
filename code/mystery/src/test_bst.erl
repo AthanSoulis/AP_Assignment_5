80692604ffae07b719ccbd37c145a13af2fc74da
Yannick-Neubert <yannick.neubert@gmx.de>
2022-10-13 17:39:59 +0200

    symbolic calls

--- code/mystery/src/test_bst.erl
+++ code/mystery/src/test_bst.erl
@@ -28,32 +28,30 @@ bst(Key, Value) ->
     ?LET(KVS, eqc_gen:list({Key, Value}),
          %{call, lists, foldl, [...]}
          %lists:foldl(
-         {call, lists, foldl, [            fun({K,V}, T) -> {call, bst, insert, [K, V, T]} end,
+         {call, lists, foldl, [            
+                    fun({K,V}, T) -> {call, bst, insert, [K, V, T]} end,
                      {call, bst, empty, []},
                      KVS]}
-                    %)
                     ).
 
+
+
+%%% we are unsure why eval needs to be used twice in the properties
+%%% we think it might be because of the nested symbolic call inside the genereator
 
 %%% invariant properties
 
 % all generated bst are valid
 prop_arbitrary_valid() ->
-    ?FORALL(ST, bst(atom_key(), int_value()),
-        begin
-            T = eval(ST),
-            valid(eqc_symbolic:eval(T))
-        end).
+    ?FORALL(T, bst(atom_key(), int_value()),
+            valid(eval(eval(T)))).
 
 % if we insert into a valid tree it stays valid
 prop_insert_valid() ->
-    ?FORALL({SK, SV, ST}, {atom_key(), int_value(), bst(atom_key(), int_value())},
-        begin
-            K = eval(SK),
-            V = eval(SV),
-            T = eval(ST),
-            valid (insert(eqc_symbolic:eval(K), eqc_symbolic:eval(V), eqc_symbolic:eval(T)))
-        end).
+    ?FORALL({K, V, T}, {atom_key(), int_value(), bst(atom_key(), int_value())},
+            valid (insert(eval(eval(K)), eval(eval(V)), eval(eval(T))))).
+
+%should also stay valid for other operations
 
 
 
@@ -62,22 +60,21 @@ prop_insert_valid() ->
 prop_insert_post() ->
     ?FORALL({K1, K2, V, T},
             {atom_key(), atom_key(), int_value(), bst(atom_key(), int_value())},
-            eqc:equals(find(eqc_symbolic:eval(K2), insert(eqc_symbolic:eval(K1), eqc_symbolic:eval(V), eqc_symbolic:eval(T))),
-                       case eqc_symbolic:eval(K1) =:= eqc_symbolic:eval(K2) of
-                           true ->  {found, eqc_symbolic:eval(V)};
-                           false -> find(eqc_symbolic:eval(K2), eqc_symbolic:eval(T))
+            eqc:equals(find(eval(eval(K2)), insert(eval(eval(K1)), eval(eval(V)), eval(eval(T)))),
+                       case eval(eval(K1)) =:= eval(eval(K2)) of
+                           true ->  {found, eval(eval(V))};
+                           false -> find(eval(eval(K2)), eval(eval(T)))
                        end)).
-
 
 prop_find_post_present() ->
   % ∀ k v t. find k (insert k v t) === {found, v}
     ?FORALL({K, V, T}, {atom_key(), int_value(), bst(atom_key(), int_value())},
-            eqc:equals(find(eqc_symbolic:eval(K), insert(eqc_symbolic:eval(K), eqc_symbolic:eval(V), eqc_symbolic:eval(T))),
-                       {found, eqc_symbolic:eval(V)})).
-
+            eqc:equals(find(eval(eval(K)), insert(eval(eval(K)), eval(eval(V)), eval(eval(T)))),
+                       {found, eval(eval(V))})).
 
 prop_find_post_absent() -> true.
      % ∀ k t. find k (delete k t) === nothing
+
 
 
 %%% -- metamorphic properties
@@ -86,32 +83,30 @@ prop_find_post_absent() -> true.
 prop_size_insert() ->
     % ∀ k v t. size (insert k v t) >= size t
     ?FORALL({K, V, T}, {atom_key(), int_value(), bst(atom_key(), int_value())},
-            bst:size(insert(eqc_symbolic:eval(K), eqc_symbolic:eval(V), eqc_symbolic:eval(T))) >= bst:size(eqc_symbolic:eval(T))).
-
-
+            bst:size(insert(eval(eval(K)), eval(eval(V)), eval(eval(T)))) >= bst:size(eval(eval(T)))).
 
 obs_equals(T1, T2) ->
-     eqc:equals(to_sorted_list(eqc_symbolic:eval(T1)), to_sorted_list(eqc_symbolic:eval(T2))).
+     eqc:equals(to_sorted_list(eval(eval(T1))), to_sorted_list(eval(eval(T2)))).
 
 prop_insert_insert() ->
     ?FORALL({K1, K2, V1, V2, T},
             {atom_key(), atom_key(), int_value(), int_value(),
              bst(atom_key(), int_value())},
-            obs_equals(insert(eqc_symbolic:eval(K1), eqc_symbolic:eval(V1), insert(eqc_symbolic:eval(K2), eqc_symbolic:eval(V2), eqc_symbolic:eval(T))),
-                       case eqc_symbolic:eval(K1) =:= eqc_symbolic:eval(K2) of
-                           true ->  insert(eqc_symbolic:eval(K1), eqc_symbolic:eval(V1), eqc_symbolic:eval(T));
-                           false -> insert(eqc_symbolic:eval(K2), eqc_symbolic:eval(V2), insert(eqc_symbolic:eval(K1), eqc_symbolic:eval(V1), eqc_symbolic:eval(T)))
+            obs_equals(insert(eval(eval(K1)), eval(eval(V1)), insert(eval(eval(K2)), eval(eval(V2)), eval(eval(T)))),
+                       case eval(eval(K1)) =:= eval(eval(K2)) of
+                           true ->  insert(eval(eval(K1)), eval(eval(V1)), eval(eval(T)));
+                           false -> insert(eval(eval(K2)), eval(eval(V2)), insert(eval(eval(K1)), eval(eval(V1)), eval(eval(T))))
                        end)).
+
 
 
 %%% -- Model based properties
 model(T) -> to_sorted_list(T).
 
-
 prop_insert_model() ->
     ?FORALL({K, V, T}, {atom_key(), int_value(), bst(atom_key(), int_value())},
-            equals(model(insert(eqc_symbolic:eval(K), eqc_symbolic:eval(V), eqc_symbolic:eval(T))),
-                   sorted_insert(eqc_symbolic:eval(K), eqc_symbolic:eval(V), delete_key(eqc_symbolic:eval(K), model(eqc_symbolic:eval(T)))))).
+            equals(model(insert(eval(eval(K)), eval(eval(V)), eval(eval(T)))),
+                   sorted_insert(eval(eval(K)), eval(eval(V)), delete_key(eval(eval(K)), model(eval(eval(T))))))).
 
 
 -spec delete_key(Key, [{Key, Value}]) -> [{Key, Value}].
